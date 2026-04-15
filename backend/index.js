6efadb7ed4e32d6e74b1c4743a7f923049974dd7
ankurkushwaha809 <ankurkushwaha809@gmail.com>
2026-04-15 13:56:58 +0530

    check backend 3

--- backend/index.js
+++ backend/index.js
@@ -85,7 +85,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Test Route
 app.get("/", (req, res) => {
-  res.send("CI/CD deployed successfully00🚀");
+  res.send("CI/CD deployed successfully🚀");
 });
 // app.get("/", (req, res) => {
 //   res.send("Server is running  on port 5000🚀");
